c7fe382b61ac6f5209fb3d1cd95975883a58a0e2
Kevin Winter <kcgwinter@gmail.com>
2025-11-29 00:02:10 -0500

    change position app swagger

--- Program.cs
+++ Program.cs
@@ -12,11 +12,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
-app.UseSwaggerUi(options =>
-{
-    options.DocumentPath = "/openapi/v1.json";
-});
 
 app.UseHttpsRedirection();
 
