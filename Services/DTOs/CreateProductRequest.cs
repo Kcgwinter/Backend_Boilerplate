faa9f614428ac1968e7ca3d8026521d169b4fca0
Kevin Winter <kcgwinter@gmail.com>
2025-11-28 23:56:31 -0500

    reset

--- Services/DTOs/CreateProductRequest.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Backend_Boilerplate.DTOs;
-
-public class CreateProductRequest
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
--- Services/ICurrentTenantService.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Backend_Boilerplate.Services;
-
-public interface ICurrentTenantService
-{
-    string? TenantId { get; set; }
-    public Task<bool> SetTenant(string tenant);
-}
